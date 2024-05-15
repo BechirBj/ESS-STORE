8ff5ac142bab4b27fa658a7289b5d4b2d3ecdd19
Bechir Ben Jazia <bechir.benjazia8@gmail.com>
2024-05-15 22:46:42 +0100

    Second commit

---
+++ src/Components/Header/navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './navbar.css'; 
+import logo from '../../Assets/Nouveau dossier/Logo_store.jpg'
+import search from '../../Assets/navbar/search.png';
+import panier from '../../Assets/navbar/panier.png';
+import profile from '../../Assets/navbar/profile.png';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='header-top'>
+        <p>
+          Sign up to become an ETOILISTE and get 15% off your first purchase <span> <a href=""> Sign up </a></span>
+        </p>
+      </div>
+
+      <div className="navigation-bar">
+        <header className='relative'>
+          <nav className='nav-top'>
+            <div className='logo-section'>
+              <div>
+                <img src={logo} alt="ESS-Store"  />
+              </div>
+            </div>
+            <div className='search-section'>
+              <div className='search-box'>      
+                <img src={search} alt="Search" />
+                <div className="inputGroup">
+                  <input type="text" required="" autoComplete="off" />
+                  <label htmlFor="name">Search</label>
+                </div>
+              </div>
+            </div>
+            <div className='icons-box'>
+              <div>
+                en-eur
+              </div>
+              <div className='icons'>
+                <button>
+                  <img src={profile} alt="Profile" />
+                </button>
+                <button>
+                  <img src={panier} alt="Panier" />
+                </button>
+              </div>
+            </div>
+          </nav>
+        </header>
+      </div>
+    </div>  
+  );
+}
+
+export default Navbar;
