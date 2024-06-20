6cc56292eef9ac48725889fb1a4cbe0c6dd6ba42
Bechir Ben Jazia <bechir.benjazia8@gmail.com>
2024-06-20 23:37:12 +0100

    Player 1

--- src/Components/ShopPlayers/ShopPlSection.jsx
+++ src/Components/ShopPlayers/ShopPlSection.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import allPlayers from '../../Assets/Data/Players/Players'; // Assuming allPlayers is imported correctly
-import { Link, useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 
 const ShopPlSection = () => {
     const [filter, setFilter] = useState('All'); 
